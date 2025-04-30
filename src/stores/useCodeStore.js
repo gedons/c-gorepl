ced7dddfb4c3b6d8590bb71174f018e0d5c26c55
Nerdy Gedoni <gedoniani@gmail.com>
2025-04-30 15:38:01 +0100

    fix: include stdin in API request and update response handling in useCodeStore

--- src/stores/useCodeStore.js
+++ src/stores/useCodeStore.js
@@ -1,22 +1,27 @@
-import { defineStore } from 'pinia';
-import axios from 'axios';
+// stores/code.js
+import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useCodeStore = defineStore('code', {
   state: () => ({
     code: `\n// You can edit this code!\n// Click here and start typing.\npackage main\n\nimport "fmt"\n\nfunc main() {\n  fmt.Println("Hello, world!")\n}`,
     output: '',
     error: '',
-    loading: false
+    loading: false,
+    stdin: '', 
   }),
   actions: {
     async runCode() {
       this.loading = true
       this.output = ''
-      this.error  = ''
+      this.error = ''
       try {
-        const resp = await axios.post('/api/run', { code: this.code, stdin: this.stdin });
-        this.output = resp.data.output
-        this.error  = resp.data.error
+        const resp = await axios.post('/api/run', {
+          code: this.code,
+          stdin: this.stdin, 
+        })
+        this.output = resp.data.stdout
+        this.error = resp.data.stderr
       } catch (e) {
         this.error = e.message
       } finally {
